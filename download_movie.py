0c2f0cb1fb029589da299d33ab512c2daf5ae6d3
TakatoKamimura <takato718@outlook.com>
2023-04-11 12:44:12 +0900

    add file

---
+++ download_movie.py
@@ -0,0 +1,9 @@
+from yt_dlp import YoutubeDL
+
+# ダウンロード条件を設定する。今回は画質・音質ともに最高の動画をダウンロードする
+ydl_opts = {'format': 'best',
+            'outtmpl': 'movie\%(title)s.%(ext)s'}
+
+# 動画のURLを指定
+with YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=YmI5FK1ANl0'])
